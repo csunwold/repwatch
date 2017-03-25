febddd6dd71b12382481df30f23ffb033e42b082
Corey Sunwold <csunwold@gmail.com>
2017-03-25 10:38:27 -0700

    Added state apply

--- domain/src/main/scala/org/repwatch/models/State.scala
+++ domain/src/main/scala/org/repwatch/models/State.scala
@@ -1,5 +1,68 @@
 package org.repwatch.models
 
-class State(val value: String) extends AnyVal {
+import scala.collection.immutable.HashSet
 
+object State {
+  val States = HashSet("AL",
+    "AK",
+    "AZ",
+    "AR",
+    "CA",
+    "CO",
+    "CT",
+    "DE",
+    "DC",
+    "FL",
+    "GA",
+    "HI",
+    "ID",
+    "IL",
+    "IN",
+    "IA",
+    "KS",
+    "KY",
+    "LA",
+    "ME",
+    "MD",
+    "MA",
+    "MI",
+    "MN",
+    "MS",
+    "MO",
+    "MT",
+    "NE",
+    "NV",
+    "NH",
+    "NJ",
+    "NM",
+    "NY",
+    "NC",
+    "ND",
+    "OH",
+    "OK",
+    "OR",
+    "PA",
+    "RI",
+    "SC",
+    "SD",
+    "TN",
+    "TX",
+    "UT",
+    "VT",
+    "VA",
+    "WA",
+    "WV",
+    "WI",
+    "WY"
+  )
+
+  def apply(code: String): Option[State] = {
+    if (code == null || !States.contains(code)) {
+      None
+    } else {
+      Some(new State(code))
+    }
+  }
 }
+
+class State(val code: String) extends AnyVal
