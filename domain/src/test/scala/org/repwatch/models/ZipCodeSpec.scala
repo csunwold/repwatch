93986d41545c58a182335d7a24cec8f10c3c543a
Corey Sunwold <csunwold@gmail.com>
2017-04-08 10:15:00 -0700

    Added tests and improved zip code validation

---
+++ domain/src/test/scala/org/repwatch/models/ZipCodeSpec.scala
@@ -0,0 +1,27 @@
+package org.repwatch.models
+
+import org.scalatest.{FlatSpec, Matchers, OptionValues}
+
+class ZipCodeSpec extends FlatSpec with Matchers with OptionValues {
+  behavior of "apply"
+
+  it should "return None if value is null" in {
+    ZipCode(null) should be (None)
+  }
+
+  it should "return None if value is empty string" in {
+    ZipCode("") should be (None)
+  }
+
+  it should "return None if value is only white space" in {
+    ZipCode("    ") should be (None)
+  }
+
+  it should "return None if value is invalid zip code" in {
+    ZipCode("3") should be (None)
+  }
+
+  it should "return Some(ZipCode) if value is valid zip code" in {
+    ZipCode("20500").value.value should be ("20500")
+  }
+}
